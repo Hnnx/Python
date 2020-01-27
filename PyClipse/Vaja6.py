32b1e852f6603cf4c23f281f7fc0700eef382b09
HNNX <nejc.stibilj01@gmail.com>
2020-01-27 20:03:10 +0100

    concat + add

---
+++ PyClipse/Vaja6.py
@@ -0,0 +1,16 @@
+#Metoda
+def sestej(a, b):
+    rezultat = a+b
+    return rezultat
+
+izracun = sestej(3,10)
+print(izracun)
+
+#Lahko tui concat
+concat = sestej("con", "cat")
+print(concat)
+
+
+
+
+    
